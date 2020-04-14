1ff7c467a95c8db37b45fe2106ffd7ad5a2c07ba
mydnac <mandycoelho5@gmail.com>
2020-04-14 17:00:42 +0200

    feat(stepper): change items, encapsulation CSS none, import MessageService

--- src/app/pages/components/stepper/stepper.component.ts
+++ src/app/pages/components/stepper/stepper.component.ts
@@ -1,24 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import {MenuItem} from 'primeng/api';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {MenuItem, MessageService} from 'primeng/api';
 
 @Component({
   selector: 'btd-stepper',
   templateUrl: './stepper.component.html',
-  styleUrls: ['./stepper.component.scss']
+  styleUrls: ['./stepper.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [MessageService],
 })
 export class StepperComponent implements OnInit {
 
   items: MenuItem[];
+  activeIndex = 1;
+  constructor(private messageService: MessageService) {}
 
-  constructor() { }
-
-    ngOnInit() {
-        this.items = [
-            {label: 'Algo 1'},
-            {label: 'Algo 2'},
-            {label: 'Algo 3'},
-            {label: 'Algo 4'},
-            {label: 'Algo 5'}
-        ];
-    }
+  ngOnInit() {
+    this.items = [{
+      command: (event: any) => {
+          this.activeIndex = 0;
+          this.messageService.add({severity: 'info', summary: 'Algo 1', detail: event.item.label});
+      }
+  },
+  {
+      command: (event: any) => {
+          this.activeIndex = 1;
+          this.messageService.add({severity: 'info', summary: 'Algo 2', detail: event.item.label});
+      }
+  },
+  {
+      command: (event: any) => {
+          this.activeIndex = 2;
+          this.messageService.add({severity: 'info', summary: 'Algo 3', detail: event.item.label});
+      }
+  },
+  {
+      command: (event: any) => {
+          this.activeIndex = 3;
+          this.messageService.add({severity: 'info', summary: 'Algo 4', detail: event.item.label});
+      }
+  },
+  {
+    command: (event: any) => {
+        this.activeIndex = 4;
+        this.messageService.add({severity: 'info', summary: 'Algo 5', detail: event.item.label});
+      }
+  }
+];
+}
 }
