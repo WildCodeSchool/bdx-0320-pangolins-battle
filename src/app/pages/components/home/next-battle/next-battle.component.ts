3580975d3a05f14bc63ea08011b7a5bad443f21c
Fabien <jouanneau.fab@gmail.com>
2020-04-15 11:07:43 +0200

    Feat: create  elements in next-battle component

--- src/app/pages/components/home/next-battle/next-battle.component.ts
+++ src/app/pages/components/home/next-battle/next-battle.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./next-battle.component.scss']
 })
 export class NextBattleComponent implements OnInit {
-
+  title = 'Next Battle';
   constructor() { }
 
   ngOnInit(): void {
