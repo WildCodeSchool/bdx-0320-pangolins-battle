a7d6bc680a2f878c08361e595b7ea1dea4296a95
mydnac <mandycoelho5@gmail.com>
2020-04-27 23:27:55 +0200

    feat(algo-admin): add method editAlgo for put algo in API

--- src/app/shared/services/algorithm/algorithm.service.ts
+++ src/app/shared/services/algorithm/algorithm.service.ts
@@ -15,7 +15,7 @@ export class AlgorithmService {
     new Algo(5, 'tu dois afficher les nombres paires [7, 6, 5, 4, 3, 2, 1]', [7, 6, 5, 4, 3, 2, 1], '[6,4,2]'),
   ];
 
-  private BASE_URL = 'http://api.witpoc.com/algos';
+  public BASE_URL = 'http://api.witpoc.com/algos';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -44,6 +44,10 @@ export class AlgorithmService {
   // requêtes http mandy
   getAllAlgo(): Observable<object>{
     return this.httpClient.get<object>(this.BASE_URL);
+  }
+
+  editAlgo(algo: any){
+    return this.httpClient.put(this.BASE_URL + `/${algo.id.toString()}`, algo);
   }
 
 }
