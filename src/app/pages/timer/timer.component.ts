4d63e2a67408878318562bf6740075ca4f47a106
Fabien <jouanneau.fab@gmail.com>
2020-04-27 15:23:39 +0200

    feat(timer): change timer depending on API

--- src/app/pages/timer/timer.component.ts
+++ src/app/pages/timer/timer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { BattlesListService } from 'src/app/shared/services/battles-list/battles-list.service';
 
 @Component({
   selector: 'btd-timer',
@@ -7,47 +8,59 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
   constructor() { }
+  @Input() battleList: any[];
+
   time: number; days: number; hours: number; minutes: number; seconds: number;
 
   startDate = Date.now();
-  endDate = '2020/04/24'; // futur input
-  battleHour = 21; // futur input
-  battleMinute = 41; // futur input
+  endDate = [];
+  battleHour = 19; // futur input ou mis en dur ?
+  battleMinute = 0; // futur input ou mis en dur ?
+  resolutionDelay = 24;
 
-  battleEndDate = Date.parse(this.endDate) + (this.battleHour * 3600 * 1000) + (this.battleMinute * 60 * 1000);
-
+  battleEndDate: number;
 
   @Output() timerOut = new EventEmitter();
   timesOut = true;
 
-
-
-  nextBattleTimer(startingDate, endingDate) {
-    this.time = endingDate - startingDate;
-    this.seconds = Math.floor( (this.time / 1000) % 60 );
-    this.minutes = Math.floor( (this.time / 1000 / 60) % 60 );
-    this.hours = Math.floor( (this.time / (1000 * 60 * 60)) % 24 );
-    this.days = Math.floor( this.time / (1000 * 60 * 60 * 24) );
-    return {time: this.time, days: this.days, hours: this.hours, minutes: this.minutes, seconds: this.seconds };
-  }
-
-  changeHiddenValue(){
-    if (Date.now() >= this.battleEndDate){
-      this.timesOut = false;
+    nextBattleTimer(startingDate, endingDate) {
+      this.time = endingDate - startingDate;
+      this.seconds = Math.floor( (this.time / 1000) % 60 );
+      this.minutes = Math.floor( (this.time / 1000 / 60) % 60 );
+      this.hours = Math.floor( (this.time / (1000 * 60 * 60)) % 24 );
+      this.days = Math.floor( this.time / (1000 * 60 * 60 * 24) );
+      return {time: this.time, days: this.days, hours: this.hours, minutes: this.minutes, seconds: this.seconds };
     }
-    return this.timerOut.emit(this.timesOut);
-  }
 
-  ngOnInit(): void {
-    setInterval(() => {
-      if (Date.now() >= this.battleEndDate){
-        this.startDate = this.battleEndDate;
-      } else {
-        this.startDate = Date.now();
+    changeHiddenValue(endDate){
+      if (Date.now() >= endDate){
+        this.timesOut = false;
       }
-      this.nextBattleTimer(this.startDate, this.battleEndDate);
-      this.changeHiddenValue();
-    }, 100);
-  }
+      return this.timerOut.emit(this.timesOut);
+    }
 
-}
+    selectNextBattle(){
+      this.endDate = this.battleList
+      .filter(battle => (+battle.launchDate > (Date.now() - this.resolutionDelay * 3600 * 1000)))
+      .sort((a, b) => (a.launchDate) - (b.launchDate));
+      // tslint:disable-next-line: max-line-length
+      return this.battleEndDate = (this.endDate[0].launchDate - 2 * 3600 * 1000) + (this.battleHour * 3600 * 1000) + (this.battleMinute * 60 * 1000);
+    }
+
+
+    ngOnInit(): void {
+      setTimeout(() => {
+        this.selectNextBattle();
+        setInterval(() => {
+          if (Date.now() >= this.battleEndDate){
+            this.startDate = this.battleEndDate;
+          } else {
+            this.startDate = Date.now();
+          }
+          this.nextBattleTimer(this.startDate, this.battleEndDate);
+          this.changeHiddenValue(this.battleEndDate);
+        }, 100);
+
+      }, 500);
+    }
+  }
