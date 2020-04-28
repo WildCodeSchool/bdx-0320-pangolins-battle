421275e8cf83d35c95388cb151cc2031110de88d
mydnac <mandycoelho5@gmail.com>
2020-04-28 11:38:44 +0200

    feat(algo-admin): add output changedAlgo

--- src/app/pages/algo-admin/algo-form/algo-form.component.ts
+++ src/app/pages/algo-admin/algo-form/algo-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { NewAlgo } from '../../../classes/new-algo';
-import { AlgorithmService } from '../../../shared/services/algorithm/algorithm.service';
 
 @Component({
   selector: 'btd-algo-form',
@@ -11,60 +10,34 @@ export class AlgoFormComponent implements OnInit {
 
   display = true;
   algos: NewAlgo [] = [];
+  finalAlgo = {};
 
   @Input() isHidden;
 
   @Input() editedAlgo;
 
-  // en attende de solution avec Hugo
-  // @Output() changedAlgo = new EventEmitter();
+  @Output() changedAlgo = new EventEmitter();
 
-  // finalAlgo = {};
-  constructor(private editAlgoService: AlgorithmService) { }
-
-  initializeAlgoList(){
-    const algos$ = this.editAlgoService.getAllAlgo();
-    algos$.subscribe((algos: NewAlgo[]) => this.algos = algos);
-  }
+  constructor() { }
 
   ngOnInit(): void {
+    this.editedAlgo = {id: 0, level: 0, instructions: '', skeleton: '', solution: ''};
   }
-
 
   displayForm(){
     this.display = this.isHidden;
     console.log(this.display);
   }
 
-  //   donneesFormVersParent(){
-  //   // Manque le level dans l'API + Title inutile + Manque skeleton
-  //   const finalAlgo = {
-  //     id: this.editedAlgo.id,
-  //     title: this.editedAlgo.skeleton,
-  //     solution: this.editedAlgo.solution,
-  //     instructions: this.editedAlgo.instructions};
-
-  //   console.log(typeof finalAlgo.id);
-  //   console.log(this.editAlgoService.BASE_URL);
-  //   console.log(finalAlgo.id.toString());
-  //   console.log(this.editAlgoService.BASE_URL + finalAlgo.id.toString());
-
-  //   this.editAlgoService.editAlgo(finalAlgo).subscribe(() => {
-  //     this.initializeAlgoList();
-  //   });
-  //   console.log(finalAlgo);
-  //   this.displayForm();
-  // }
-  // en attente de solution avec Hugo
-  // donneesFormVersParent(){
-  //   // Manque le level dans l'API + Title inutile + Manque skeleton
-  //   this.finalAlgo = {
-  //     id: this.editedAlgo.id,
-  //     title: this.editedAlgo.skeleton,
-  //     solution: this.editedAlgo.solution,
-  //     instructions: this.editedAlgo.instructions};
-  //   this.changedAlgo.emit(this.finalAlgo);
-  //   console.log(this.finalAlgo);
-  //   this.displayForm();
-  // }
+  dataFormToParent(){
+    // Manque le level dans l'API + Title inutile + Manque skeleton
+    this.finalAlgo = {
+      id: this.editedAlgo.id,
+      title: this.editedAlgo.level,
+      solution: this.editedAlgo.solution,
+      instructions: this.editedAlgo.instructions};
+    this.changedAlgo.emit(this.finalAlgo);
+    console.log(this.finalAlgo);
+    this.displayForm(); // fonctionne pas ?
+  }
 }
