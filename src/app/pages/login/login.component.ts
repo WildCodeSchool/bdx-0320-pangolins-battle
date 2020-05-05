d95b3c5386cddcf022b7f5d8d2ec777fe6e8c27f
Fabien <jouanneau.fab@gmail.com>
2020-05-05 14:35:03 +0200

    fix(login): fix timer

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/shared/services/user/user.service';
 import { User } from 'src/app/classes/user';
+import { BattlesListService } from '../../shared/services/battles-list/battles-list.service';
 
 @Component({
   selector: 'btd-login',
@@ -12,13 +13,20 @@ export class LoginComponent implements OnInit {
   hidden = true;
   user: User;
   timesOut = true;
-  constructor(private route: ActivatedRoute, private userService: UserService) { }
+  battleList = [];
+  constructor(private route: ActivatedRoute, private userService: UserService, private battleAPI: BattlesListService) { }
+
+  initializePage(){
+    this.battleAPI.getAllBattles()
+    .subscribe(data => this.battleList = data);
+  }
 
   display(timesOut: boolean){
     return this.hidden = timesOut;
   }
 
   ngOnInit(): void {
+    this.initializePage();
     this.display(this.hidden);
 // par ce paramMap, je récupère la partie 'token' de l'URL. Je le fais au moment où
 // L'utilisateur clique sur "connexion avec mon compte Odyssey".
